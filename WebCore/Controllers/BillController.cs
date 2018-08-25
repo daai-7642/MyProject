3dc24e912a47113916ebd720bfede745fe4bb32e
wcf <764297968@qq.com>
2018-08-25 17:13:38 +0800

    api

---
+++ WebCore/Controllers/BillController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebCore.Models;
+
+namespace WebCore.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/Bill")]
+    public class BillController : Controller
+    {
+        BillContext _context;
+        public BillController(BillContext billcontext)
+        {
+            _context = billcontext;
+        }
+        AB_Logic.ChargeInfo_Logic _Logic = new AB_Logic.ChargeInfo_Logic();
+
+        [HttpPost]
+        public IActionResult Post([FromBody]ChargeInfo charge)
+        {
+            charge.CreateTime = DateTime.Now;
+            _context.ChargeInfo.Add(charge);
+            int result = _context.SaveChanges();
+            //int result = _Logic.AddChargeInfo(charge);
+            return CreatedAtRoute("bill", result);
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return "hello world";
+        }
+
+    }
+}
---
+++ WebCore/Models/ChargeInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCore.Models
+{ 
+    public class ChargeInfo
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Key]
+        public System.Guid AccountBookId { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public System.DateTime CreateTime { get; set; } 
+        /// <summary>
+        /// 金额
+        /// </summary>
+        public decimal Money { get; set; }
+        /// <summary>
+        /// 消费时间
+        /// </summary>
+        public DateTime ChargeTime { get; set; }
+       
+        /// <summary>
+        /// 详细地址
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 相关人
+        /// </summary>
+        public string RelatedPeople { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string RemarkInfo { get; set; }
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public string TypeId { get; set; }
+        /// <summary>
+        /// 录入人
+        /// </summary>
+        public string CreateAuthor { get; set; } 
+    }
+}
