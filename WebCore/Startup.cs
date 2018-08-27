c302b270c7abd068c6187a843051e9980e81e873
wcf <764297968@qq.com>
2018-08-27 20:14:17 +0800

    0

--- WebCore/Startup.cs
+++ WebCore/Startup.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,13 +47,7 @@ namespace WebCore
             //{
             //    app.UseDeveloperExceptionPage();
             //}
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
-
-            app.UseAuthentication();
-
+           
             app.UseCors(builder=> {
                 builder.AllowAnyOrigin();
                 builder.AllowAnyHeader();
